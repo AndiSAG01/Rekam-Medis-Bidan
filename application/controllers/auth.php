0f12d667f38ccd1b2dd8670fe3be35ede1caba39
adptra01 <m.a.syaputra.@gmail.com>
2023-02-03 00:32:01 +0700

    loginSelesai

--- application/controllers/auth.php
+++ application/controllers/auth.php
@@ -14,40 +14,52 @@ class auth extends CI_Controller
             $this->load->view('v_login');
     }
     public function login_aksi()
-    {
-        $user = $this->input->post('username', true);
-        $pass = md5 ($this->input->post('password', true));
+{
+    $user = $this->input->post('username', true);
+    $pass = md5 ($this->input->post('password', true));
 
-        //rule validasi
-        $this->form_validation->set_rules('username','username','required');
-        $this->form_validation->set_rules('password','password','required');
+    //rule validasi
+    $this->form_validation->set_rules('username','username','required');
+    $this->form_validation->set_rules('password','password','required');
 
 
-        if($this->form_validation->run() != FALSE){
+    if($this->form_validation->run() != FALSE){
 
-            $where = array (
-                'username' => $user,
-                'password' => $pass
-            );
+        $where = array (
+            'username' => $user,
+            'password' => $pass
+        );
 
-            $ceklogin = $this->m_login->cek_login($where)->num_rows();
+        $ceklogin = $this->m_login->cek_login($where)->num_rows();
 
-            if($ceklogin > 0 ){
+        if($ceklogin > 0 ){
+            $data = $this->m_login->cek_login($where)->row();
+
+            if ($data->hak_akses == 'pemilik') {
                 $sess_data = array(
-
                     'login'=>'OK',
-                    'username'=> $user 
+                    'username'=> $user,
+                    'hak_akses' => 'pemilik' 
                 );
-
                 $this->session->set_userdata($sess_data);
                 redirect(base_url());;
-            } else{
-                redirect('auth');
+            } else if ($data->hak_akses == 'asisten') {
+                $sess_data = array(
+                    'login'=>'OK',
+                    'username'=> $user,
+                    'hak_akses' => 'asisten' 
+                );
+                $this->session->set_userdata($sess_data);
+                    $this->load->view('asisten');
             }
-        }else {
-            $this->load-view('v_login');
+        } else{
+            redirect('auth');
         }
+    }else {
+        $this->load-view('v_login');
     }
+}
+
     
 
     function logout(){
